81c7ea9bae6019d2da354ca8b3debcb86e6decf8
acidsasha <sohan.boxan@gmail.com>
2024-10-23 16:43:19 +0300

    inital commit

---
+++ TestAppBalina/TestAppBalina/TestAppBalinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppBalinaApp.swift
+//  TestAppBalina
+//
+//  Created by Alexandr Filovets on 23.10.24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppBalinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
